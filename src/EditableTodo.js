72354fd0755c6dca724b999f4b2f22a2939f5f6f
Maurice <maurice.baez1@gmail.com>
2022-04-28 15:12:51 -0700

    initial commit, fixed toDo, editableToDo components

---
+++ src/EditableTodo.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import Todo from "./Todo";
+import TodoForm from "./TodoForm";
+
+/** Show editable todo item.
+ *
+ * Props
+ * - todo
+ * - update(): fn to call to update a todo
+ * - remove(): fn to call to remove a todo
+ *
+ * EditableTodoList -> EditableTodo -> { Todo, TodoForm }
+ */
+
+function EditableTodo({ todo, update, remove }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  /** Toggle if this is being edited */
+  function toggleEdit() {
+    setIsEditing(true);
+  }
+
+  /** Call remove fn passed to this. */
+  function handleDelete(id) {
+    remove(id);
+  }
+
+  /** Edit form saved; toggle isEditing and update in ancestor. */
+  function handleSave(formData) {
+    update(formData);
+    setIsEditing(false);
+  }
+
+  return (
+    <div className="EditableTodo">
+      {isEditing ? (
+        <TodoForm />
+      ) : (
+        <div className="mb-3">
+          <div className="float-end text-sm-end">
+            <button
+              className="EditableTodo-toggle btn-link btn btn-sm"
+              onClick={toggleEdit}
+            >
+              Edit
+            </button>
+            <button
+              className="EditableTodo-delBtn btn-link btn btn-sm text-danger"
+              onClick={() => handleDelete(todo.id)}
+            >
+              Del
+            </button>
+          </div>
+          <Todo />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default EditableTodo;
